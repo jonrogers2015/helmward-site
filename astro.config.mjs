6e57df80ae184801e239c49d3ce556158aec2c30
Jon Rogers <jonrogers2015@gmail.com>
2026-06-21 16:27:13 +0000

    fix: remove sitemap integration causing build failure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://calm-concha-4f483f.netlify.app',
-  integrations: [sitemap()],
 });
